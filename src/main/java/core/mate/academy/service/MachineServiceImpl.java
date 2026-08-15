b6dde4389443de466f3482ccb175b73cf9c409ed
Liza Brnk <liza.brnk@gmail.com>
2026-08-15 20:17:10 +0300

    added implementation in MachineServiceImpl class, created a list of specific machines (Bulldozer, Excavator, and Truck), changed the parameteration.

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,49 @@
 package core.mate.academy.service;
 
+import core.mate.academy.model.Bulldozer;
+import core.mate.academy.model.Excavator;
+import core.mate.academy.model.Machine;
+import core.mate.academy.model.Truck;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Your implementation of MachineService.
  */
-public class MachineServiceImpl {
+public class MachineServiceImpl<T extends Machine>
+        implements MachineService<T>, MachineProducer<T> {
+    private BulldozerProducer bulldozerProducer = new BulldozerProducer();
+    private ExcavatorProducer excavatorProducer = new ExcavatorProducer();
+    private TruckProducer truckProducer = new TruckProducer();
+
+    @Override
+    public List<T> get() {
+        return List.of();
+    }
+
+    @Override
+    public List<T> getAll(Class type) {
+        if (type == Bulldozer.class) {
+            return (List<T>) bulldozerProducer.get();
+        } else if (type == Excavator.class) {
+            return (List<T>) excavatorProducer.get();
+        } else if (type == Truck.class) {
+            return (List<T>) truckProducer.get();
+        }
+        return new ArrayList<>();
+    }
+
+    @Override
+    public void fill(List<? super T> machines, T value) {
+        for (int i = 0; i < machines.size(); i++) {
+            machines.set(i, value);
+        }
+    }
+
+    @Override
+    public void startWorking(List<? extends T> machines) {
+        for (T machine : machines) {
+            machine.doWork();
+        }
+    }
 }
